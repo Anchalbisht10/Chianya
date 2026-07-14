dd58bf1cafda8cd0c32cf1307bb2864c9fc3e13b
Anchal bisht <anchal001bisht@gmail.com>
2026-07-14 14:24:09 +0530

    update

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,13 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
 });
-
 async function sendResetEmail(email, resetLink) {
   await transporter.sendMail({
     from: `"Chianya" <${process.env.EMAIL_USER}>`,
